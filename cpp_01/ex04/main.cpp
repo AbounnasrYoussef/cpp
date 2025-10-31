431f73a3ea36ce4c76001bc0b110c949b31a5a63
AbounnasrYoussef <abounnasryoussef@gmail.com>
2025-10-31 17:53:57 +0100

    ex04

---
+++ cpp_01/ex04/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+
+
+
+
+int main(int ac , char **av)
+{
+    if(ac != 4)
+    {
+        std::cerr<< "error , entrer 3 parametre" << std::endl;
+        return 1;
+    }
+    else 
+    {
+        std::string file = av[1];
+        std::string s1 = av[2];
+        std::string s2 = av[3];
+
+        if(s1.empty())
+        {
+            std::cerr<< "error , s1 ne peut pas etres vide !"<< std::endl;
+            return 1;
+        }
+
+        std::ifstream inputFile(file);
+        if(!inputFile.is_open())
+        {
+            std::cerr<< "error , impossible d'ouvrir le fichier !" << std::endl;
+            return 1;
+        }
+
+        std::ofstream outputFile(file + ".replace");
+        if(!outputFile.is_open())
+        {
+            std::cout<< "error , impossible de creer le fichier !" << std::endl;
+            return 1;
+        }
+        // Lire tout le contenu du fichier dans une string
+        std::string content((std::istreambuf_iterator<char>(inputFile)),
+                         std::istreambuf_iterator<char>());
+
+         // Fermer le fichier d’entrée (bonne pratique)
+        inputFile.close();
+
+    // Remplacer toutes les occurrences de s1 par s2
+        size_t pos = content.find(s1);
+        while (pos != std::string::npos)
+        {
+            content = content.substr(0, pos) + s2 + content.substr(pos + s1.length());
+            pos = content.find(s1, pos + s2.length());
+        }
+
+    // Écrire le contenu modifié dans le fichier de sortie
+        outputFile << content;
+
+     // Fermer le fichier de sortie
+        outputFile.close();
+
+        std::cout << "✅ Remplacement terminé avec succès ! Fichier créé : "
+              << file + ".replace" << std::endl;
+
+
+
+
+    }
+    return 0;
+}
