bdd28b75545519ec7c6f295a08b67eefcd34cda2
AbounnasrYoussef <abounnasryoussef@gmail.com>
2025-11-19 09:58:11 +0100

    ex01 cpp04

--- cpp_04/ex01/main.cpp
+++ cpp_04/ex01/main.cpp
@@ -4,21 +4,9 @@
 
 int main()
 {
-    Animal* animals[4];
-
-    for (int i = 0; i < 4; i++)
-    {
-        if (i % 2 == 0)
-            animals[i] = new Dog();
-        else
-            animals[i] = new Cat();
-    }
-
-    for (int i = 0; i < 4; i++)
-        animals[i]->makeSound();
-
-    for (int i = 0; i < 4; i++)
-        delete animals[i];
-
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    delete j;//should not create a leak
+    delete i;
     return 0;
 }
