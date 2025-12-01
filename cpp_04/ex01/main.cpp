e5c90c8e7080da99a4eb3eab94cc3dd37f3f11d4
AbounnasrYoussef <abounnasryoussef@gmail.com>
2025-12-01 10:57:14 +0100

    cpp05 debut

--- cpp_04/ex01/main.cpp
+++ cpp_04/ex01/main.cpp
@@ -4,8 +4,11 @@
 
 int main()
 {
+    Brain b;
     const Animal* j = new Dog();
     const Animal* i = new Cat();
+    b.setIdea(99, "Coder le CPP04");
+    std::cout << "Idea 0: " << b.getIdea(0) << std::endl;
     delete j;
     delete i;
     return 0;
