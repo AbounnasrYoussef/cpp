c506dd0b8ddb6c786b7c9c70fb0cddaee52df71a
AbounnasrYoussef <abounnasryoussef@gmail.com>
2025-12-01 11:26:39 +0100

    bureaucrat

---
+++ cpp_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,25 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+
+
+class Bureaucrat
+{
+private:
+    const std::string nom;
+    int grade;
+public:
+    Bureaucrat();
+    ~Bureaucrat();
+};
+
+
+
+
+
+
+
+
+
+#endif
