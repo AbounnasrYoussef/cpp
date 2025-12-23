4c8506453217a0319378cfdc08b9bb78ca420c1e
AbounnasrYoussef <abounnasryoussef@gmail.com>
2025-12-23 11:21:34 +0100

    exeption

--- cpp_05/ex00/Bureaucrat.hpp
+++ cpp_05/ex00/Bureaucrat.hpp
@@ -2,6 +2,7 @@
 #define BUREAUCRAT_HPP
 
 #include <string>
+#include <exception>
 
 
 class Bureaucrat
@@ -11,14 +12,37 @@ private:
     int grade;
 public:
     Bureaucrat();
+
+    Bureaucrat(std::string &nom, int grade);
+
+    Bureaucrat(const Bureaucrat &other);
+
+    Bureaucrat &operator=(const Bureaucrat &other);
+    
+    const std::string &getName() const;
+
+    int getGrade() const;
+
+    void incrementGrade();
+    
+    void decrementGrade();
+
+    class GradeTooHighException : public std::exception
+    {
+    public:
+        const char *what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+    public:
+        const char *what() const throw();
+    };
+
     ~Bureaucrat();
 };
 
-
-
-
-
-
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat);
 
 
 
